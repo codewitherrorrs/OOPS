7877f5a3b7a1d02fc3548aac48f075688ee561f9
“codewitherrors” <codewitherrors1@gmail.com>
2023-06-26 10:39:02 +0530

    Add : Polymorphism Example

---
+++ OOPS/OOPS/PolymorphismDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class PolymorphismDemo
+    {
+       public void sound()
+        {
+            Console.WriteLine("The Sound of Parent is wwww");
+        }
+    }
+
+    public class Child : PolymorphismDemo
+    {
+        public void sound()
+        {
+            Console.WriteLine("The Sound of Child is hohohoh");
+        }
+    }
+}
--- OOPS/OOPS/Program.cs
+++ OOPS/OOPS/Program.cs
@@ -4,16 +4,21 @@
     {
         public static void Main(string[] args)
         {
-            ObjectAndClass obj = new ObjectAndClass();
-            obj.Sum(20, 30);
+            PolymorphismDemo pd = new PolymorphismDemo();
+            pd.sound();
+            Child cd = new Child();
+            cd.sound();
 
-            Inherit inherit= new Inherit();
+            /*ObjectAndClass obj = new ObjectAndClass();
+            obj.Sum(20, 30);*/
+
+            /*Inherit inherit= new Inherit();
             inherit.addition(30, 30);
             inherit.addition(25.3, 30.65);
-            /*inherit.addition(3.030, 23.030);
+            inherit.addition(3.030, 23.030);
             inherit.addition("Hello"," World");
-            Console.WriteLine(inherit.addition())*/
-            inherit.Sum(50,50);
+            Console.WriteLine(inherit.addition());
+            inherit.Sum(50,50);*/
         }
     }
 }
