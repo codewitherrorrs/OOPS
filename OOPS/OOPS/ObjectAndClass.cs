72a7d0ed82660c0db29413991f741852d472eb9e
“codewitherrors” <codewitherrors1@gmail.com>
2023-06-26 10:22:29 +0530

    Add : Types of Variable

--- OOPS/OOPS/ObjectAndClass.cs
+++ OOPS/OOPS/ObjectAndClass.cs
@@ -12,6 +12,35 @@ namespace OOPS
         {
             Console.WriteLine($"The Addition of a and b : {a+b}");
         }
+
+        public void addition(float a, float b)
+        {
+            Console.WriteLine($"The Addition of a and b : {a + b}");
+        }
+
+        public void addition(double a, double b)
+        {
+            Console.WriteLine($"The Addition of a and b : {a + b}");
+        }
+
+        public void addition(string a, string b)
+        {
+            Console.WriteLine($"The Addition of a and b : {a + b}");
+        }
+
+        public string addition()
+        {
+            bool a = true;
+            bool b = false;
+            if (a == b)
+            {
+               return "True";
+            }
+            else
+            {
+               return "False";
+            }
+        }
     }
 
     class Inherit : ObjectAndClass
--- OOPS/OOPS/Program.cs
+++ OOPS/OOPS/Program.cs
@@ -9,6 +9,10 @@
 
             Inherit inherit= new Inherit();
             inherit.addition(30, 30);
+            inherit.addition(25.3, 30.65);
+            inherit.addition(3.030, 23.030);
+            inherit.addition("Hello"," World");
+            Console.WriteLine(inherit.addition());
         }
     }
 }
