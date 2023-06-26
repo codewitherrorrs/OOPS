28bd66f8a5e1ff70d72e008aa978cff41d2a1f76
“codewitherrors” <codewitherrors1@gmail.com>
2023-06-26 10:04:23 +0530

    Add : Object And Classes

---
+++ OOPS/OOPS/ObjectAndClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class ObjectAndClass
+    {
+        public void addition(int a,int b)
+        {
+            Console.WriteLine($"The Addition of a and b : {a+b}");
+        }
+    }
+}
---
+++ OOPS/OOPS/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ObjectAndClass obj = new ObjectAndClass();
+            obj.addition(20, 30);
+        }
+    }
+}
